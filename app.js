2fe217462aac0becfd9436bba13e13c8cf4b0bb2
qianzhong516 <zhongqian516@gmail.com>
2023-07-18 22:54:37 +1000

    fix indent - disable prettier.useTabs

--- app.js
+++ app.js
@@ -2,16 +2,16 @@ import React from 'react';
 import { useState } from 'react';
 
 export const App = () => {
-	const [counter, setCounter] = useState(0);
+    const [counter, setCounter] = useState(0);
 
-	const onClick = () => {
-		setCounter((prev) => ++prev);
-	};
+    const onClick = () => {
+        setCounter((prev) => ++prev);
+    };
 
-	return (
-		<div>
-			<h1>Counter: {counter}</h1>
-			<button onClick={onClick}>Increase Counter</button>
-		</div>
-	);
+    return (
+        <div>
+            <h1>Counter: {counter}</h1>
+            <button onClick={onClick}>Increase Counter</button>
+        </div>
+    );
 };
