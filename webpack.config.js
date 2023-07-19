a8302b5f4df56edbee7f451ce308cb7bff3ca017
qianzhong516 <zhongqian516@gmail.com>
2023-07-19 17:36:47 +1000

    add HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
@@ -7,6 +8,13 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Create React From Scratch',
+            meta: { viewport: 'width=device-width, initial-scale=1.0' },
+            template: './index.html',
+        }),
+    ],
     module: {
         rules: [
             {
